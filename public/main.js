822b5a20d96b4e9b5d60adc840b535dd95c2856e
James Perih <james@hotdang.ca>
2020-05-29 18:17:55 -0600

    Add expandable icon.

--- public/main.js
+++ public/main.js
@@ -7,7 +7,14 @@ const plusButton = document.getElementById('plus');
 const minusButton = document.getElementById('minus');
 const countElement = document.getElementById('count');
 const tenantTextbox = document.getElementById('tenant');
+const expandButton = document.getElementById('expand-button');
 
+const instructionsDiv = document.getElementById('instructions');
+const mastHeadDiv = document.getElementById('masthead');
+const footerDiv = document.getElementsByTagName('footer')[0];
+console.log(footerDiv);
+
+let isExpanded = false;
 let count = 0;
 let tenant = '';
 
@@ -16,7 +23,12 @@ tenantTextbox.addEventListener('keyup', (e) => {
     requestNewData();
 });
 
-plusButton.addEventListener('click', (e) => {
+expandButton.addEventListener('click', () => {
+    isExpanded = !isExpanded;
+    expandOrCollapseInterface();
+});
+
+plusButton.addEventListener('click', (_) => {
     if (!tenant || tenant === '') {
         alert('Set your location first!');
         return;
@@ -34,6 +46,13 @@ minusButton.addEventListener('click', (e) => {
     count = Math.max(count - 1, 0);
     socket.emit('count', { tenant, count });
 });
+
+function expandOrCollapseInterface() {
+    const style = isExpanded ? 'none' : 'flex';
+    mastHeadDiv.style.display = style;
+    instructionsDiv.style.display = style;
+    footerDiv.style.display = style;
+}
 
 function displayCount() {
     countElement.innerHTML = count;
